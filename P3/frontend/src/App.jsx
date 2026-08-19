de15b4301a2b1dc41a10630d9267fca4be030d3c
xeniumhs <thexenium07@gmail.com>
2026-08-19 20:03:24 +0545

    frontend push

---
+++ P3/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import SystemStatus from "./components/SystemStatus";
+
+function App() {
+  return (
+    <div>
+      <h1>AI Interview System</h1>
+
+      <SystemStatus />
+    </div>
+  );
+}
+
+export default App;
